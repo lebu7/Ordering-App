e15c118b81a1a1c664b789a72c63b6fd6da22369
Lebu7 <35328371+lebu7@users.noreply.github.com>
2024-05-08 19:45:00 +0300

    selectedOption stamp on Orders

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -18,18 +18,34 @@ export default function OrdersPage() {
     return (
         <section className="mt-8 max-w-2xl mx-auto">
             <UserTabs isAdmin={profile.admin} />
-            <div className="mt-8 max-w-2xl mx-auto">
+            <div className="mt-8 max-w-xl mx-auto">
                 {orders?.length > 0 && orders.map(order => (
                     <div className="bg-gray-100 rounded-lg py-3 px-4 grid grid-cols-5 gap-2 mb-2 text-xs" key={order._id}>
                         <div>
-                            <span className="font-semibold">{order.orderId ? order.orderId : <span className="text-primary">PayOnDelivery</span>}</span>
+                            {order.orderId ? (
+                                <span className="font-semibold">{order.orderId}</span>
+                            ) : (
+                                <span className="text-gray-600 font-semibold">PayOnDelivery</span>
+                            )}
                         </div>
-                
-                
                         <div className="px-0">
                             {order?.cartProducts.reduce((total, product) => 
                                 product.colours ? total + product.colours.length : total, 0)} Items
+                            {order.selectedOption && (
+                                <span className="ml-2 text-right border border-black rounded-sm px-1 font-semibold">
+                                    {order.selectedOption === 'Courier Service' && (
+                                        <span className="text-orange-600">C</span>
+                                    )}
+                                    {order.selectedOption === 'Door Delivery' && (
+                                        <span className="text-primary">D</span>
+                                    )}
+                                    {order.selectedOption === 'CBD Pickup' && (
+                                        <span className="text-green-600">P</span>
+                                    )}
+                                </span>
+                            )}
                         </div>
+
                         <div className="text-gray-500 text-center"> 
                             {order.userEmail}
                         </div>
